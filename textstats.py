756bf1d98705d223820357732fa7fc57be75fcbc
Jacklack3 <13090766+Jacklack3@users.noreply.github.com>
2017-08-05 18:11:15 -0400

    Removed line of code that was useless.

--- textstats.py
+++ textstats.py
@@ -1,6 +1,5 @@
 import re, colorama, sys, pip
 from colorama import Fore, Back, Style, init
-init()
 init(autoreset=True)
 installed_packages = pip.get_installed_distributions()
 flat_installed_packages = [package.project_name for package in installed_packages]
